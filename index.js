5f98619873e40b2272bc5839fef9dd21f3402fbc
hanomag123 <87242345+hanomag123@users.noreply.github.com>
2022-07-24 15:09:55 +0300

    last

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
 import express from "express";
-import jwt from 'jsonwebtoken';
+import * as PostController from './controllers/PostController.js'
+import multer from 'multer'
+
+import mongoose from "mongoose";
+import {postCreateValidation, registerValidator} from './validations/auth.js'
+
+
+
+import checkAuth from "./utils/checkAuth.js";
+import { getMe, login, register } from "./controllers/UserController.js";
+import handkeValidationErrors from "./utils/handkeValidationErrors.js";
+
+mongoose.connect('mongodb+srv://han:12345@cluster0.evarq.mongodb.net/blog?retryWrites=true&w=majority')
+.then(() => console.log('DB ok'))
+.catch(err => {console.log('DB err: ', err)})
 
 const app = express()
 const PORT = 4444;
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+const upload = multer({
+  storage
 })
 
 app.use(express.json())
-app.post('/auth/login', (req, res) => {
-  const token = jwt.sign({
-    email: req.body.email,
-    fullname: 'Vasya Pupkin'
-  }, 'secret123')
-  console.log(req.body)
+app.use('/uploads', express.static('uploads'))
+
+app.post('/upload',checkAuth, upload.single('image'), (req, res) => {
   res.json({
-    success: true,
-    token
+    url: `/uploads/${req.file.originalname}`
   })
 })
 
+app.post('/auth/login', registerValidator,handkeValidationErrors, login)
+
+app.post('/auth/register', registerValidator, handkeValidationErrors, register)
+app.get('/auth/me', checkAuth, getMe)
+app.get('/posts/getAll', PostController.getAll)
+app.get('/posts/:id', PostController.getOne)
+app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+app.delete('/posts/:id',checkAuth, PostController.remove)
+app.patch('/posts/:id',checkAuth, postCreateValidation, PostController.update)
 app.listen(PORT, (err) => {
   if (err) {
     return console.log(err)
---
+++ validations/auth.js
@@ -0,0 +1,15 @@
+import {body} from 'express-validator'
+
+export const registerValidator = [
+  body('email', 'Wrong Email').isEmail(),
+  body('password', 'Wrong Password').isStrongPassword(),
+  body('fullName', 'Wrong fullname').isLength({min: 3}),
+  body('avatarUrl', 'Not an').optional().isURL()
+]
+
+export const postCreateValidation = [
+  body('title', 'Inter the title of article').isLength({min: 3}).isString(),
+  body('text', 'Inter the text of article').isLength({min: 10}).isString(),
+  body('tags', 'Inter the right tag of article').optional().isString(),
+body('imageUrl', 'Incorrect link of the image').optional().isString()
+]
